b2ea6cc03425647673b5fa3cacd38b300e4b4f0f
kthejaswini6-dot <kthejaswini6@gmail.com>
2026-03-29 11:07:29 +0000

    Pipeline

--- Pipeline/ingest_data.py
+++ Pipeline/ingest_data.py
@@ -1,19 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 
 import pandas as pd
 from sqlalchemy import create_engine
-
-year = 2021
-month = 1
-
-
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-
-
-# In[3]:
-
+from tqdm.auto import tqdm
 
 dtype = {
     "VendorID": "Int64",
@@ -38,61 +26,3 @@ parse_dates = [
     "tpep_pickup_datetime",
     "tpep_dropoff_datetime"
 ]
-
-df = pd.read_csv(
-    prefix + 'yellow_tripdata_2021-01.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates
-)
-
-engine = create_engine('postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}: {pg_port}/{pg_db}',pool_pre_ping=True)
-
-
-# In[12]:
-
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    chunksize=100000
-
-    df_iter = pd.read_csv(
-        prefix + 'yellow_tripdata_2021-01.csv.gz',
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize
-    )
-
-    first = True
-    for df_chunk in df_iter:
-        if first:
-            df.head(n=0).to_sql(
-                name='yellow_taxi_data', 
-                con=engine, 
-                if_exists='replace')
-            first = False
-    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
-    print("Table created")
-
-    # Insert chunk
-    df_chunk.to_sql(
-        name="yellow_taxi_data",
-        con=engine,
-        if_exists="append"
-    )
-
-
-
-print("Inserted:", len(df_chunk))
-
-if __name__ == '__main__':
-    run()
-
-# In[ ]:
-
-
-
-
